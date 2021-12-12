98ce30c34c2a3c7058ccec5814967ae34c14a202
ana-os-mo <anmosoriomo@unal.edu.co>
2021-12-11 19:05:18 -0500

    feat: docker-compose file

---
+++ protractor /local.config.ts
@@ -0,0 +1,43 @@
+import { browser, Config } from 'protractor';
+
+const firefoxConfig = {
+  browserName: 'firefox',
+  firefoxOptions: {
+    args: ['--headless']
+  },
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: ['--headless', '--disable-gpu', '--window-size=1920,1080']
+  },
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
+export const config: Config = {
+  multiCapabilities,
+  framework: 'mocha',
+  specs: [ '../test/ui/**/*.js'],
+  seleniumAddress: 'http://0.0.0.0:4444',
+  SELENIUM_PROMISE_MANAGER : false,
+  mochaOpts: {
+    timeout: 18000,
+    reporter: 'mochawesome-screenshots',
+    reporterOptions: {
+      reportName: "report",
+      multiReport: true,
+    }
+  },
+  getPageTimeout: 30000,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+  }
+};
