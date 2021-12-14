884a144373dbe5eb8f2d09b0734ae776f7e96fff
Salome Aristizabal Giraldo <53347835+salitomeyo@users.noreply.github.com>
2021-12-14 02:35:58 -0500

    trying to fix add items #8

--- src/page/addItem.page.ts
+++ src/page/addItem.page.ts
@@ -2,21 +2,21 @@ import { $, ElementFinder } from 'protractor';
 
 export class AddItemPage {
   private item1: ElementFinder;
-  private item2: ElementFinder;
-  private item3: ElementFinder;
+  //private item2: ElementFinder;
+  //private item3: ElementFinder;
   private checkoutButton: ElementFinder;
 
   constructor() {
-    this.item1 = $('.productListWrapper > div:nth-child(1) > .tile > .titleBottom > .tileAdd > button');
-    this.item2 = $('.productListWrapper > div:nth-child(6) > .tile > .titleBottom > .tileAdd > button');
-    this.item3 = $('.productListWrapper > div:nth-child(7) > .tile > .titleBottom > .tileAdd > button');
+    this.item1 = $('.tileAdd > button');
+    //this.item2 = $('.productListWrapper > div:nth-child(6) > .tile > .titleBottom > .tileAdd > button');
+    //this.item3 = $('.productListWrapper > div:nth-child(7) > .tile > .titleBottom > .tileAdd > button');
     this.checkoutButton = $('.checkout-button > a')    
 }
 
   public async addItems(): Promise<void> {
     await this.item1.click();
-    await this.item2.click();
-    await this.item3.click();
+    //await this.item2.click();
+    //await this.item3.click();
   }
 
   public async proceedToCheckout(): Promise<void> {
