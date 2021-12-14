01e06bf0087ec4c226678b8259d0ea8a6d7878b0
Ana Osorio <88041760+ana-os-mo@users.noreply.github.com>
2021-12-14 02:56:51 -0500

    Update env.ts

--- src/testObjects/env.ts
+++ src/testObjects/env.ts
@@ -1,11 +1,11 @@
 const url = {
-    // "base_url":'http://18.118.253.100:8080/',
-    // "base_url_api":"http://18.118.253.100:8080/api",
-    // "base_ui_url": "http://18.118.253.100:8080"
+    "base_url":'http://18.118.253.100:8080/',
+    "base_url_api":"http://18.118.253.100:8080/api",
+    "base_ui_url": "http://18.118.253.100:8080"
     // "base_ui_url": "http://host.docker.internal:8080"
-    "base_url":`${process.env.PUBLIC_DNS}:8080/`,
-    "base_url_api":`${process.env.PUBLIC_DNS}:8080/api`,
-    "base_ui_url": `${process.env.PUBLIC_DNS}:8080`
+//     "base_url":`${process.env.PUBLIC_DNS}:8080/`,
+//     "base_url_api":`${process.env.PUBLIC_DNS}:8080/api`,
+//     "base_ui_url": `${process.env.PUBLIC_DNS}:8080`
 };
 
 exports.url = url;
