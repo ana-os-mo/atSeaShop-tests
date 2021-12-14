ed090368b181d5cf54064d4c1cd96ead0170b0ba
ana-os-mo <anmosoriomo@unal.edu.co>
2021-12-14 00:03:52 -0500

    last

--- src/testObjects/env.ts
+++ src/testObjects/env.ts
@@ -1,7 +1,8 @@
 const url = {
-    "base_url":'http://localhost:8080',
-    "base_url_api":"http://localhost:8080/api",
-    "base_ui_url": "http://host.docker.internal:8080"
+    "base_url":'http://18.118.253.100:8080/',
+    "base_url_api":"http://18.118.253.100:8080/api",
+    "base_ui_url": "http://18.118.253.100:8080"
+    //"base_ui_url": "http://host.docker.internal:8080"
 };
 
 exports.url = url;
