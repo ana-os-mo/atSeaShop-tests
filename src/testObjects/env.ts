b1adadefe223d4571cb800c112f513fcc70da5e2
Ana Osorio <88041760+ana-os-mo@users.noreply.github.com>
2021-12-14 02:45:56 -0500

    Update env.ts

--- src/testObjects/env.ts
+++ src/testObjects/env.ts
@@ -3,9 +3,9 @@ const url = {
     // "base_url_api":"http://18.118.253.100:8080/api",
     // "base_ui_url": "http://18.118.253.100:8080"
     // "base_ui_url": "http://host.docker.internal:8080"
-    "base_url":'ec2-18-118-253-100.us-east-2.compute.amazonaws.com:8080/',
-    "base_url_api":"ec2-18-118-253-100.us-east-2.compute.amazonaws.com:8080/api",
-    "base_ui_url": "ec2-18-118-253-100.us-east-2.compute.amazonaws.com:8080"
+    "base_url":`${process.env.PUBLIC_DNS}:8080/`,
+    "base_url_api":`${process.env.PUBLIC_DNS}:8080/api`,
+    "base_ui_url": `${process.env.PUBLIC_DNS}:8080`
 };
 
 exports.url = url;
