59fdc04862fa10cd0095f60269377e5340cd247a
Salome Aristizabal Giraldo <53347835+salitomeyo@users.noreply.github.com>
2021-12-13 23:28:50 -0500

    ui url as env variable

--- src/testObjects/env.ts
+++ src/testObjects/env.ts
@@ -1,6 +1,7 @@
 const url = {
     "base_url":'http://localhost:8080',
-    "base_url_api":"http://localhost:8080/api"
+    "base_url_api":"http://localhost:8080/api",
+    "base_ui_url": "http://host.docker.internal:8080"
 };
 
 exports.url = url;
